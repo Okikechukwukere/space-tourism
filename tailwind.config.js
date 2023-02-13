ab4d1edb184fa66e1d3e7133f77ee220fe7e659d
Okikechukwukere <okikegideon@gmail.com>
2023-02-14 03:08:43 +0800

    updated the url

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,24 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-tab": "url('./src/assets/home/background-home-tablet.jpg')",
-        "hero-desk": "url('./src/assets/home/background-home-desktop.jpg')",
-        "hero-mob": "url('./src/assets/home/background-home-mobile.jpg')",
+        "hero-tab": "url('./assets/home/background-home-tablet.jpg')",
+        "hero-desk": "url('./assets/home/background-home-desktop.jpg')",
+        "hero-mob": "url('./assets/home/background-home-mobile.jpg')",
         "dest-mob":
-          "url('/src/assets/destination/background-destination-mobile.jpg')",
+          "url('.assets/destination/background-destination-mobile.jpg')",
         "dest-tab":
-          "url('/src/assets/destination/background-destination-tablet.jpg')",
+          "url('./assets/destination/background-destination-tablet.jpg')",
         "dest-desk":
-          "url('/src/assets/destination/background-destination-desktop.jpg')",
-        "crew-mob": "url('/src/assets/crew/background-crew-mobile.jpg')",
-        "crew-tab": "url('/src/assets/crew/background-crew-tablet.jpg')",
-        "crew-desk": "url('/src/assets/crew/background-crew-desktop.jpg')",
+          "url('./assets/destination/background-destination-desktop.jpg')",
+        "crew-mob": "url('./assets/crew/background-crew-mobile.jpg')",
+        "crew-tab": "url('./assets/crew/background-crew-tablet.jpg')",
+        "crew-desk": "url('./assets/crew/background-crew-desktop.jpg')",
         "tech-mob":
-          "url('/src/assets/technology/background-technology-mobile.jpg')",
+          "url('./assets/technology/background-technology-mobile.jpg')",
         "tech-tab":
-          "url('/src/assets/technology/background-technology-tablet.jpg')",
+          "url('./assets/technology/background-technology-tablet.jpg')",
         "tech-desk":
-          "url('/src/assets/technology/background-technology-desktop.jpg')",
+          "url('./assets/technology/background-technology-desktop.jpg')",
       },
       colors: {
         "nav-color": "rgba(150,150,150,0.15)",
